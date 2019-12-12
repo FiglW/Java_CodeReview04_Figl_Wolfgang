def955e3cf5dd06d6c3b5e88ed7c77f64ae376f3
Wolfgang Figl <wolfgang.figl@me.com>
2019-12-12 22:21:37 +0100

    add images, and little structure

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,16 +4,39 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.InputStream;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        InputStream input = this.getClass().getResourceAsStream("" );
+        Image image = new Image(input);
+        ImageView imageView = new ImageView(image);
+
+        // mit BorderPane versuchen
+
+        BorderPane root = new BorderPane();
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
+
+
+        // Vbox,Hbox oida
+
+
+
+
+        //images
+
     }
 
 
--- src/sample/Product.java
+++ src/sample/Product.java
@@ -1,4 +1,70 @@
 package sample;
 
+import javafx.scene.image.Image;
+
 public class Product {
+    String name;
+    String quantity;
+    String description;
+    double oldPrice;
+    double newPrice;
+    Image image;
+
+    public Product(String name, String quantity, String description, double oldPrice, double newPrice, Image image) {
+        this.name = name;
+        this.quantity = quantity;
+        this.description = description;
+        this.oldPrice = oldPrice;
+        this.newPrice = newPrice;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getOldPrice() {
+        return oldPrice;
+    }
+
+    public void setOldPrice(double oldPrice) {
+        this.oldPrice = oldPrice;
+    }
+
+    public double getNewPrice() {
+        return newPrice;
+    }
+
+    public void setNewPrice(double newPrice) {
+        this.newPrice = newPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", oldPrice=" + oldPrice +
+                ", newPrice=" + newPrice +
+                '}';
+    }
 }
